a699fcfe5a9b5df1b51fcec8e568451f37896a6b
Asifur Rahman <asifurrahman.ac@gmail.com>
2025-06-15 02:33:46 +0600

    mongodb added

---
+++ src/lib/mongoConnect.js
@@ -0,0 +1,13 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+export default function mongoDb(collection_name) {
+  const uri = process.env.MONGODB_URI;
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  });
+  return client.db(process.env.DB_NAME).collection(collection_name);
+}
