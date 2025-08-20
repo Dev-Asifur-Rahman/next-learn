c13b7c24a7694fd339d75e05a29a1beff184cf78
Asifur Rahman <asifurrahman.ac@gmail.com>
2025-08-20 19:32:35 +0600

    enorlled courses api under processing

--- src/app/api/student/analytics/route.js
+++ src/app/api/student/analytics/route.js
@@ -1,6 +1,16 @@
+import mongoDb, { collections } from "@/lib/mongoConnect";
+import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
+// show analytics about students
 export async function GET(req) {
-    return NextResponse.json({success:true,message:'Api Found'
-    })
+  const token = await getToken({ req });
+  const email = token.email;
+  const students = await mongoDb(collections.student);
+  const student = await students.findOne({ email: email });
+  if (!student)
+    return NextResponse.json({ success: false, message: "Student not Found" });
+else{
+    return NextResponse.json(student)
+}
 }
