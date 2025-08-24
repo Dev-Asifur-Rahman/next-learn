e2de2e86a8dbe414f03bb0542a3503938d025a9d
Asifur Rahman <asifurrahman.ac@gmail.com>
2025-08-24 22:11:35 +0600

    readme updated

---
+++ public/icons/react-icons.js
@@ -0,0 +1,21 @@
+import { IoIosAnalytics } from "react-icons/io";
+import { RiAdminFill } from "react-icons/ri";
+import { FaChalkboardTeacher } from "react-icons/fa";
+import { PiStudentBold } from "react-icons/pi";
+import { CgProfile } from "react-icons/cg";
+import { MdDashboardCustomize } from "react-icons/md";
+import { SiCoursera } from "react-icons/si";
+
+
+
+export const icons = {
+    analytics : <IoIosAnalytics />,
+    admins : <RiAdminFill />,
+    instructors : <FaChalkboardTeacher />,
+    students : <PiStudentBold />,
+    profile : <CgProfile />,
+    dashboard : <MdDashboardCustomize />,
+    course : <SiCoursera />
+}
+
+
