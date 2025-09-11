258e6b2c82133c69fc88bd9035f49d2e9bed3d76
Asifur Rahman <asifurrahman.ac@gmail.com>
2025-09-11 23:12:08 +0600

    working on promtion user api

--- src/app/api/admin/promotion/route.js
+++ src/app/api/admin/promotion/route.js
@@ -1,5 +1,9 @@
+import mongoDb, { collections } from "@/lib/mongoConnect";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-    return NextResponse.json({success:true})
+  const students = await mongoDb(collections.student);
+  const instructors = await mongoDb(collections.instructor);
+  const admins = await mongoDb(collections.admin);
+  return NextResponse.json({ success: true });
 }
