2ad3922ff0dc5d982ab96c00564ed08e7deabb0f
Asifur Rahman <asifurrahman.ac@gmail.com>
2025-09-12 23:27:00 +0600

    working on promotion api

--- src/app/api/admin/promotion/route.js
+++ src/app/api/admin/promotion/route.js
@@ -1,9 +1,36 @@
 import mongoDb, { collections } from "@/lib/mongoConnect";
+import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 
-export async function GET() {
+// promotion user instructor or admin 
+export async function GET(req) {
+  const serverSession = await getServerSession()
   const students = await mongoDb(collections.student);
   const instructors = await mongoDb(collections.instructor);
   const admins = await mongoDb(collections.admin);
+
+  const checkRole = serverSession.user.role
+  // all codes will be under this condition 
+  if(checkRole === 'admin'){
+
+  }
+  
+  const queries = req.nextUrl.searchParams;
+  
+  // get the role from query 
+  const role = queries.get('role')
+  if(role === 'student'){
+     
+  }
+
+  else if(role === 'instructor'){
+
+  }
+
+  else if(role === 'admin'){
+
+  }
+
+  const currentRole = queries.currentRole
   return NextResponse.json({ success: true });
 }
