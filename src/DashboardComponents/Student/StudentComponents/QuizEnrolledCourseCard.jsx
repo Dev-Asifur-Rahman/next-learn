1ec0690992fe64185ae46bd5cd96fc107181bdad
Asifur Rahman <asifurrahman.ac@gmail.com>
2025-10-04 22:46:58 +0600

    certifcate integration on courses in working

--- src/DashboardComponents/Student/StudentComponents/QuizEnrolledCourseCard.jsx
+++ src/DashboardComponents/Student/StudentComponents/QuizEnrolledCourseCard.jsx
@@ -1,9 +1,25 @@
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const QuizEnrolledCoursesCard = ({ course }) => {
   const { title, courseImage, _id } = course;
+  const pathname = usePathname();
+
+  const getCertificate = async () => {
+    toast.success("Congratulations! You will be notified Soon");
+
+    try {
+      const response = await axios.post("/api/student/certificates", {
+        name: "Asifur Rahman",
+        courseName: "Html Course",
+        date: "29-09-2025",
+      });
+    } catch (error) {
+      toast.error("Something Went Wrong! Try Again");
+    }
+  };
   return (
     <section className="w-full flex justify-center">
       <div className="card w-48 dark:border-4 dark:border-white text-black dark:text-white rounded-2xl dark:shadow-white/15 dark:hover:shadow-white/35 hover:scale-105 shadow-xl transition-transform duration-300">
@@ -21,11 +37,17 @@ const QuizEnrolledCoursesCard = ({ course }) => {
         <div className="card-body items-center text-center">
           <h2 className=" text-base font-bold w-full truncate">{title}</h2>
           <div className=" w-full">
-            <Link href={`/quiz/${_id}`}>
-              <button className="btn btn-sm w-full mt-4 bg-white dark:bg-transparent border dark:border-white  dark:text-white text-black ">
-                Generate Quiz
+            {pathname === "/certificate" ? (
+              <button className="btn btn-sm w-full mt-4 bg-white dark:bg-transparent border dark:border-white  dark:text-white text-black " onClick={getCertificate}>
+                Certificate
               </button>
-            </Link>
+            ) : (
+              <Link href={`/quiz/${_id}`}>
+                <button className="btn btn-sm w-full mt-4 bg-white dark:bg-transparent border dark:border-white  dark:text-white text-black ">
+                  Generate Quiz
+                </button>
+              </Link>
+            )}
           </div>
           <div className=" w-full">
             <Link href={`/class/${_id}`}>
--- src/app/certificate/page.jsx
+++ src/app/certificate/page.jsx
@@ -1,58 +1,50 @@
 "use client";
 
+import LoadingSpinner from "@/components/LoadingSpinner";
 import QuizEnrolledCoursesCard from "@/DashboardComponents/Student/StudentComponents/QuizEnrolledCourseCard";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import toast from "react-hot-toast";
 
 const page = () => {
   const [completedCourses, setCompletedCourses] = useState([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     const fetchCompletedCourses = async () => {
       const res = await axios.get("/api/completed-user-enrolled");
       const data = res.data;
       setCompletedCourses(data?.enrolledCourses);
+      setLoading(false);
     };
     fetchCompletedCourses();
   }, []);
 
-  const getCertificate = async () => {
-    toast.success('Congratulations! You will be notified Soon')
-
-    try {
-      const response = await axios.post(
-        "/api/student/certificates",
-        {
-          name: "Asifur Rahman",
-          courseName: "Html Course",
-          date: "29-09-2025",
-        },
-      );
-    } catch (error) {
-      toast.error("Something Went Wrong! Try Again");
-    }
-  };
+  
 
   return (
     <section className="w-full">
-      <h2 className="text-xl font-semibold mt-6 mb-4">Completed Courses</h2>
-      {completedCourses.length === 0 ? (
-        <p className="text-lg py-2 px-4 lg:border-2 md:border-2 border rounded-xl  font-normal ">
-          No completed courses found.
-        </p>
+      <h2 className="text-xl font-semibold mt-6 mb-4 ml-6">
+        Completed Courses
+      </h2>
+      {loading ? (
+        <LoadingSpinner></LoadingSpinner>
       ) : (
-        <section className="grid xs:grid-cols-1 ms:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-6 mt-4">
-          {completedCourses?.map((course) => (
-            <QuizEnrolledCoursesCard
-              course={course}
-              key={course._id}
-            ></QuizEnrolledCoursesCard>
-          ))}
+        <section className="w-full">
+          {completedCourses.length === 0 ? (
+            <p className="text-lg py-2 px-4 lg:border-2 md:border-2 border rounded-xl  font-normal ">
+              No completed courses found.
+            </p>
+          ) : (
+            <section className="grid xs:grid-cols-1 ms:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-6 mt-4">
+              {completedCourses?.map((course) => (
+                <QuizEnrolledCoursesCard
+                  course={course}
+                  key={course._id}
+                ></QuizEnrolledCoursesCard>
+              ))}
+            </section>
+          )}
         </section>
       )}
-      <section className="w-full flex items-center justify-center overflow-y-scroll">
-        <button onClick={getCertificate}>Request For Certificate</button>
-      </section>
     </section>
   );
 };
