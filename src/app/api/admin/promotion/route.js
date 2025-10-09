f69a0b9065823ac3ab6f53eb548edadc905c268a
Asifur Rahman <asifurrahman.ac@gmail.com>
2025-10-09 23:10:58 +0600

    promotion role completed

--- src/app/api/admin/promotion/route.js
+++ src/app/api/admin/promotion/route.js
@@ -2,10 +2,17 @@ import mongoDb, { collections } from "@/lib/mongoConnect";
 import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 import { authOptions } from "../../auth/[...nextauth]/route";
-import { collections } from "./../../../../lib/mongoConnect";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
 
-const findLastUserId = async (prefix, collection) => {
-  const getCollection = await mongoDb(collections[collection]);
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+const findLastUserId = async (prefix, promotingCollection) => {
+  const getCollection = await mongoDb(collections[promotingCollection]);
+  // get the last id of promotion role
+
   const lastUser = await getCollection
     .aggregate([
       {
@@ -32,17 +39,83 @@ const findLastUserId = async (prefix, collection) => {
   return newId;
 };
 
-const roleSet = async (currentRole, requestedRole, collection) => {
-  const collection = await mongoDb(collections[collection]);
+const roleSet = async (currentCollection, promotingCollection, userId) => {
+  const collection = await mongoDb(collections[currentCollection]);
 
-  if (currentRole === "student") {
-    const newId = await findLastUserId("s", "student");
-  } else if (currentRole === "instructor") {
-    const newId = await findLastUserId("i", "instructor");
-  } else if (currentRole === "admin") {
-    const newId = await findLastUserId("a", "admin");
-  } else {
-    return { success: false };
+  const getPromotingCollection = await mongoDb(
+    collections[promotingCollection]
+  );
+
+  // find the user
+  const user = await collection.findOne({ userId: userId });
+
+  if (user) {
+    if (promotingCollection === "student") {
+      const newId = await findLastUserId("s", "student");
+      const result = await getPromotingCollection.insertOne({
+        userId: newId,
+        name: user?.name,
+        email: user?.email,
+        role: "student",
+        profileImage: user?.profileImage,
+        location: user?.location || null,
+        password: user?.password || null,
+        joinedAt: dayjs().tz("Asia/Dhaka").format(),
+        enrolledCourses: [],
+        certificates: [],
+      });
+
+      const delete_user = await collection.deleteOne({ userId: userId });
+
+      if (result.insertedId && delete_user.deletedCount == 1) {
+        return { success: true };
+      } else {
+        return { success: false };
+      }
+    } else if (promotingCollection === "instructor") {
+      const newId = await findLastUserId("i", "instructor");
+      const result = await getPromotingCollection.insertOne({
+        userId: newId,
+        name: user?.name,
+        email: user?.email,
+        password: user?.password || null,
+        role: "instructor",
+        bio: null,
+        specialization: null,
+        location: null,
+        joinedAt: dayjs().tz("Asia/Dhaka").format(),
+        profileImage: user?.profileImage,
+      });
+
+      const delete_user = await collection.deleteOne({ userId: userId });
+
+      if (result.insertedId && delete_user.deletedCount == 1) {
+        return { success: true };
+      } else {
+        return { success: false };
+      }
+    } else if (promotingCollection === "admin") {
+      const newId = await findLastUserId("a", "admin");
+      const result = await getPromotingCollection.insertOne({
+        userId: newId,
+        name: user?.name,
+        email: user?.email,
+        password: user?.password || null,
+        role: "admin",
+        joinedAt: dayjs().tz("Asia/Dhaka").format(),
+        profileImage: user?.profileImage,
+      });
+
+      const delete_user = await collection.deleteOne({ userId: userId });
+
+      if (result.insertedId && delete_user.deletedCount == 1) {
+        return { success: true };
+      } else {
+        return { success: false };
+      }
+    } else {
+      return { success: false, message: "user not found" };
+    }
   }
 };
 
@@ -50,7 +123,7 @@ const roleSet = async (currentRole, requestedRole, collection) => {
 export async function POST(req) {
   const serverSession = await getServerSession(authOptions);
 
-  const checkRole = await serverSession.user.role;
+  const checkRole = await serverSession?.user.role;
 
   // all codes will be under this condition
   if (checkRole === "admin") {
@@ -64,10 +137,21 @@ export async function POST(req) {
 
     const id = queries.get("userId");
 
-    if (role === "student") {
-      const result = await roleSet();
-    } else if (role === "instructor") {
-    } else if (role === "admin") {
+    if (requestedRole === "student") {
+      const result = await roleSet(role, requestedRole, id);
+      return result?.success === true
+        ? NextResponse.json({ success: true })
+        : NextResponse.json({ success: false });
+    } else if (requestedRole === "instructor") {
+      const result = await roleSet(role, requestedRole, id);
+      return result?.success === true
+        ? NextResponse.json({ success: true })
+        : NextResponse.json({ success: false });
+    } else if (requestedRole === "admin") {
+      const result = await roleSet(role, requestedRole, id);
+      return result?.success === true
+        ? NextResponse.json({ success: true })
+        : NextResponse.json({ success: false });
     }
   } else {
     return NextResponse.json({
@@ -75,5 +159,4 @@ export async function POST(req) {
       message: "Verify Your Identity First",
     });
   }
-  return NextResponse.json({ success: true });
 }
