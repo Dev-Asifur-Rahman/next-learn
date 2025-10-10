f55021fb99de0094d6f32141ccd6dd2ba79b17d6
Asifur Rahman <asifurrahman.ac@gmail.com>
2025-10-10 22:56:49 +0600

    promotion code tested and notification applied after promotion implemented

--- src/hooks/usePromotionRole.jsx
+++ src/hooks/usePromotionRole.jsx
@@ -1,9 +1,14 @@
 import axios from "axios";
+import toast from "react-hot-toast";
 
 
 const usePromotionRole = async(currentRole,requestedRole,userId) => {
+    const loading = toast.loading('Please Wait')
     const response = await axios.post(`/api/admin/promotion?role=${currentRole}&request=${requestedRole}&userId=${userId}`)
-    // return 
+    if(response.data.success){
+        toast.dismiss(loading)
+        toast.success('Promoted Sucessfully')
+    }
 };
 
 export default usePromotionRole;
